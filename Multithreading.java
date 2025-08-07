bb2b4c61785dd06dcdf53dced46db86d32a78edb
jeevan2304 <jeevanhg2003@gmail.com>
2025-08-07 16:15:53 +0530

    optionals

---
+++ Multithreading.java
@@ -0,0 +1,5 @@
+public class Multithreading {
+    public static void main(String[] args){
+
+    }
+}
---
+++ Optionals.java
@@ -0,0 +1,30 @@
+import java.util.Optional;
+
+
+class item {
+    String name ;
+
+    public item(String name){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+public class Optionals {
+    public static void main(String[] args){
+        item Item = null;
+
+        Optional<item> optionalItem = Optional.ofNullable(Item);
+
+        String name = optionalItem.map(items->Item.getName()).orElse("no item found");
+
+        System.out.println(name);
+
+
+    }
+}
+
+//nullpointerexceptipon
