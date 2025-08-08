b9ab1b66dcd127fdd8e12f75cfc400173d8c33e6
jeevan2304 <jeevanhg2003@gmail.com>
2025-08-08 15:48:40 +0530

    day-2 completed

---
+++ CompletableFutureExample.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.CompletableFuture;
+public class CompletableFutureExample {
+    public static void main(String[] args) {
+
+        CompletableFuture<Void> task = CompletableFuture.runAsync(() -> {
+            System.out.println("Task running in: " + Thread.currentThread().getName());
+        });
+
+
+        CompletableFuture<String> greeting = CompletableFuture.supplyAsync(() -> {
+            return "Hello, ";
+        }).thenApply(name -> name + "Jeevan");
+
+
+        CompletableFuture<String> first = CompletableFuture.supplyAsync(() -> "Completable");
+        CompletableFuture<String> second = CompletableFuture.supplyAsync(() -> "Future");
+        CompletableFuture<String> combined = first.thenCombine(second, (a, b) -> a + " " + b);
+
+
+        greeting.thenAccept(result -> System.out.println("Greeting: " + result));
+        combined.thenAccept(result -> System.out.println("Combined: " + result));
+
+
+        CompletableFuture<String> faulty = CompletableFuture.supplyAsync(() -> {
+            if (true) throw new RuntimeException("Oops!");
+            return "This won't return";
+        }).exceptionally(e -> "Recovered from error: " + e.getMessage());
+
+        faulty.thenAccept(System.out::println);
+
+        // Wait for all to complete
+        try { Thread.sleep(2000); } catch (InterruptedException e) {}
+    }
+}
--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -1,8 +1,10 @@
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class FunctionalProgramming {
@@ -19,9 +21,13 @@ public class FunctionalProgramming {
 
         //lambda expressions:
         Predicate<String> startwithM = name->name.startsWith("M");
+        System.out.println(startwithM.test("jeevan"));
         //method reference:
         Function<String,String> toUpper = String::toUpperCase;
         Consumer<String> printer = System.out::println;
+        Supplier<LocalDateTime> currentTimeSupplier = ()->LocalDateTime.now();
+        System.out.println(currentTimeSupplier.get());
+
 
         //Streams:
         names.stream()
